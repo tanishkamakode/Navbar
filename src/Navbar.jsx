aeb0e9971caa57403d7cd1cb75c9c77663a87952
Tanishka <makodetanishka@gmail.com>
2023-04-03 10:54:53 +0530

    Set basic navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                return (
+                  <li key={link.id}>
+                    <a href={link.url}>{link.text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
